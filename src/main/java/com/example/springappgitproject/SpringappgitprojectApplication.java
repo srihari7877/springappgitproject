0f5cb195b69d421b137ade2afc9e60760bc0e833
srihari7877 <rayudu.srihari@gmail.com>
2024-07-06 23:56:31 +0530

    Revert "changes in main file"
    This reverts commit b120d9d352c98736a6172737e84c83eb59fe8fe7.

--- src/main/java/com/example/springappgitproject/SpringappgitprojectApplication.java
+++ src/main/java/com/example/springappgitproject/SpringappgitprojectApplication.java
@@ -7,13 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringappgitprojectApplication {
 
 	public static void main(String[] args) {
-		System.out.println("11111111111111111111111111");
-		System.out.println("11111111111111111111111111");
-		System.out.println("11111111111111111111111111");
-		System.out.println("11111111111111111111111111");
-		System.out.println("11111111111111111111111111");
-		System.out.println("11111111111111111111111111");
-		System.out.println("11111111111111111111111111");
 		SpringApplication.run(SpringappgitprojectApplication.class, args);
 	}
 
