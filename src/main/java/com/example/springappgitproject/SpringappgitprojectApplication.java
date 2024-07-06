a7bcd866eec8b0c4973864d8c40b4ed9c86fcb02
srihari7877 <rayudu.srihari@gmail.com>
2024-07-07 00:15:02 +0530

    Revert "changes in main class file"
    This reverts commit 538e17f26a4250cef3451068067c69cdd6c48380.

--- src/main/java/com/example/springappgitproject/SpringappgitprojectApplication.java
+++ src/main/java/com/example/springappgitproject/SpringappgitprojectApplication.java
@@ -7,11 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringappgitprojectApplication {
 
 	public static void main(String[] args) {
-		System.out.println("22222222222222222222");
-		System.out.println("22222222222222222222");
-		System.out.println("22222222222222222222");
-		System.out.println("22222222222222222222");
-		System.out.println("22222222222222222222");
 		SpringApplication.run(SpringappgitprojectApplication.class, args);
 	}
 
