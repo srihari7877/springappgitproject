97e0e982dfa63cc7c267b299b6b0ab16acd9a446
srihari7877 <rayudu.srihari@gmail.com>
2024-07-06 21:38:10 +0530

    first commit

---
+++ src/main/java/com/example/springappgitproject/SpringappgitprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.springappgitproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringappgitprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringappgitprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springappgitproject/SpringappgitprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springappgitproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringappgitprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
