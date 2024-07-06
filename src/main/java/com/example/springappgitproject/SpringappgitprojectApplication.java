538e17f26a4250cef3451068067c69cdd6c48380
srihari7877 <rayudu.srihari@gmail.com>
2024-07-07 00:11:57 +0530

    changes in main class file

--- src/main/java/com/example/springappgitproject/SpringappgitprojectApplication.java
+++ src/main/java/com/example/springappgitproject/SpringappgitprojectApplication.java
@@ -7,6 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringappgitprojectApplication {
 
 	public static void main(String[] args) {
+		System.out.println("22222222222222222222");
+		System.out.println("22222222222222222222");
+		System.out.println("22222222222222222222");
+		System.out.println("22222222222222222222");
+		System.out.println("22222222222222222222");
 		SpringApplication.run(SpringappgitprojectApplication.class, args);
 	}
 
