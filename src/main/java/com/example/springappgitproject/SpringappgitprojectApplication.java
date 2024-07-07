76640edf3f09490c9a0400e90af508fb39e93f7c
srihari7877 <rayudu.srihari@gmail.com>
2024-07-07 14:38:01 +0530

    main files changes in develop

--- src/main/java/com/example/springappgitproject/SpringappgitprojectApplication.java
+++ src/main/java/com/example/springappgitproject/SpringappgitprojectApplication.java
@@ -7,6 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringappgitprojectApplication {
 
 	public static void main(String[] args) {
+		System.out.println("44444444444");
+		System.out.println("3333444444333333333333333");
+		System.out.println("333333444443333333333333");
+		
 		SpringApplication.run(SpringappgitprojectApplication.class, args);
 	}
 
